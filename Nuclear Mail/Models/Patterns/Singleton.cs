f01c63fc9e40115ff3d65267610dbd47b062c2a1
pechhenka <mail@pechhenka.ru>
2019-10-30 19:25:14 +0300

    Паттерн Singleton

---
+++ Nuclear Mail/Models/Patterns/Singleton.cs
@@ -0,0 +1,22 @@
+﻿namespace Nuclear_Mail.Models.Patterns
+{
+    public class Singleton<T> where T: new()
+    {
+        private static T _instance = new T();
+        private static object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance != null) return _instance;
+
+                    _instance = new T();
+                    return _instance;
+                }
+            }
+        }
+    }
+}
