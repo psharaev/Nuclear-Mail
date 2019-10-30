9057533b0afc520a3ec6b947810deabd65093a16
pechhenka <mail@pechhenka.ru>
2019-10-30 19:25:32 +0300

    Почтовый ящик клиента

---
+++ Nuclear Mail/Models/MailBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nuclear_Mail.Models.Protocols;
+
+namespace Nuclear_Mail.Models
+{
+    public class MailBox
+    {
+        public string email_address = "";
+        public string password = "";
+
+        public bool usePop3 = false;
+        public bool useImap = false;
+        public bool useSmtp = false;
+
+        public POP3 Pop3;
+        public IMAP Imap;
+        public SMTP Smtp;
+    }
+}
