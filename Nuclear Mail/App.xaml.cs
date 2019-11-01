68fd8dcd5cf023e6dac010af9d1d9d87a1325342
pechhenka <mail@pechhenka.ru>
2019-11-01 19:16:30 +0300

    Изменена точка входа в программу

--- Nuclear Mail/App.xaml.cs
+++ Nuclear Mail/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Nuclear_Mail.Models;
+using Nuclear_Mail.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +16,11 @@ namespace Nuclear_Mail
     /// </summary>
     public partial class App : Application
     {
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            Data.Instance.Initialize();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
