fb2014d8f3fdb5317bd95ea4c3947a7cb0b68e98
pechhenka <mail@pechhenka.ru>
2019-11-01 19:28:16 +0300

    Перемещение Data

---
+++ Nuclear Mail/Models/Data/Data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nuclear_Mail.Models.Patterns;
+
+namespace Nuclear_Mail.Models.Data
+{
+    public class Data : Singleton<Data>
+    {
+        private static object _lockMailBoxes = new object();
+        private List<MailBox> mailBoxes = new List<MailBox>();
+        public List<MailBox> MailBoxes
+        {
+            get => mailBoxes;
+            set
+            {
+                lock (_lockMailBoxes)
+                {
+                    mailBoxes = value;
+                }
+            }
+        }
+
+        public Data()
+        {
+            FileInfo[] MailBoxFolders = Directory.CreateDirectory(@"Data\MailBoxes").GetFiles();
+            foreach (FileInfo item in MailBoxFolders)
+            {
+                Saver.LoadPath(out MailBox mailBox, item.FullName);
+                mailBoxes.Add(mailBox);
+            }
+        }
+
+        public void CreateMailBox(in MailBox mailBox)
+        {
+            string path = $@"Data\MailBoxes\{mailBox.email_address}.dat";
+            if (File.Exists(path))
+                throw new Exception("Tакой почтовый ящик уже существует");
+            Saver.SavePath(mailBox, path);
+
+        }
+
+        public void RemoveMailBox(in MailBox mailBox)
+        {
+            Saver.RemovePath($@"Data\MailBoxes\{mailBox.email_address}.dat");
+        }
+
+        public void Initialize() { }
+    }
+}
