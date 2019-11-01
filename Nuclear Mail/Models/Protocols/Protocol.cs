18b03e0298b151be7184d74eca7ec00271990061
pechhenka <mail@pechhenka.ru>
2019-11-01 19:17:51 +0300

    Настройки по умолчанию для протоколов

--- Nuclear Mail/Models/Protocols/Protocol.cs
+++ Nuclear Mail/Models/Protocols/Protocol.cs
@@ -1,5 +1,8 @@
-﻿namespace Nuclear_Mail.Models.Protocols
+﻿using System;
+
+namespace Nuclear_Mail.Models.Protocols
 {
+    [Serializable]
     public abstract class Protocol
     {
         public PostalProtocol protocol;
@@ -10,30 +13,52 @@
 
     public enum PostalProtocol
     {
-        POP3 = 0,
+        POP = 0,
         IMAP = 1,
         SMTP = 2
     }
 
+    [Serializable]
     public abstract class InboxProtocol : Protocol
     {
         public string InboxServerName = "";
     }
+    [Serializable]
     public abstract class OutboxProtocol : Protocol
     {
         public string OutboxServerName = "";
     }
-
-    public class POP3 : InboxProtocol 
+    [Serializable]
+    public class POP : InboxProtocol 
     {
-        public new PostalProtocol protocol = PostalProtocol.POP3;
+        public new PostalProtocol protocol = PostalProtocol.POP;
+        public POP(bool useSsl = true, int Port = 110, int SslPort = 995)
+        {
+            this.useSsl = useSsl;
+            this.Port = Port;
+            this.SslPort = SslPort;
+        }
     }
+    [Serializable]
     public class IMAP : InboxProtocol 
     {
         public new PostalProtocol protocol = PostalProtocol.IMAP;
+        public IMAP(bool useSsl = true, int Port = 143, int SslPort = 993)
+        {
+            this.useSsl = useSsl;
+            this.Port = Port;
+            this.SslPort = SslPort;
+        }
     }
+    [Serializable]
     public class SMTP : OutboxProtocol
     {
         public new PostalProtocol protocol = PostalProtocol.SMTP;
+        public SMTP(bool useSsl = true, int Port = 587, int SslPort = 465)
+        {
+            this.useSsl = useSsl;
+            this.Port = Port;
+            this.SslPort = SslPort;
+        }
     }
 }
