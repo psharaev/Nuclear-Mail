b4381bbde095020ff4e61359e966e57c125bee0a
pechhenka <mail@pechhenka.ru>
2019-11-03 22:24:07 +0300

    Дополнение для MailBox

--- Nuclear Mail/Models/MailBox.cs
+++ Nuclear Mail/Models/MailBox.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -9,10 +10,12 @@ using Nuclear_Mail.Models.Protocols;
 namespace Nuclear_Mail.Models
 {
     [Serializable]
-    public class MailBox
+    public class MailBox:IComparable<MailBox>
     {
         public string email_address = "";
         public string password = "";
+
+        public string Name = "";
 
         public bool usePop3 = false;
         public bool useImap = false;
@@ -22,9 +25,41 @@ namespace Nuclear_Mail.Models
         public IMAP Imap = null;
         public SMTP Smtp = null;
 
+        public MailBox(string email_address, string password)
+        {
+            this.email_address = email_address;
+            this.password = password;
+            useImap = true;
+            useSmtp = true;
+            Pop3 = new POP(email_address: email_address);
+            Smtp = new SMTP(email_address: email_address);
+        }
+
         public MailBox()
         {
 
+        }
+
+        public int CompareTo(MailBox other)
+        {
+            if (other == null) return 1;
+            return email_address.CompareTo(other.email_address);
+        }
+        public static bool operator >(MailBox operand1, MailBox operand2)
+        {
+            return operand1.CompareTo(operand2) == 1;
+        }
+        public static bool operator <(MailBox operand1, MailBox operand2)
+        {
+            return operand1.CompareTo(operand2) == -1;
+        }
+        public static bool operator >=(MailBox operand1, MailBox operand2)
+        {
+            return operand1.CompareTo(operand2) >= 0;
+        }
+        public static bool operator <=(MailBox operand1, MailBox operand2)
+        {
+            return operand1.CompareTo(operand2) <= 0;
         }
     }
 }
