4e3fae6d8daadc1d3c1dbad509f687fd326d1bcd
pechhenka <mail@pechhenka.ru>
2019-11-03 22:25:05 +0300

    Возможность доверять любому SSL

--- Nuclear Mail/Models/Protocols/Protocol.cs
+++ Nuclear Mail/Models/Protocols/Protocol.cs
@@ -7,6 +7,7 @@ namespace Nuclear_Mail.Models.Protocols
     {
         public PostalProtocol protocol;
         public bool useSsl = false;
+        public bool TrustAnySSL = false;
         public int Port = 0;
         public int SslPort = 0;
     }
@@ -32,33 +33,36 @@ namespace Nuclear_Mail.Models.Protocols
     public class POP : InboxProtocol 
     {
         public new PostalProtocol protocol = PostalProtocol.POP;
-        public POP(bool useSsl = true, int Port = 110, int SslPort = 995)
+        public POP(bool useSsl = true, int Port = 110, int SslPort = 995,string email_address = null)
         {
             this.useSsl = useSsl;
             this.Port = Port;
             this.SslPort = SslPort;
+            if (email_address != null) InboxServerName = "pop." + email_address.Split('@')[1];
         }
     }
     [Serializable]
     public class IMAP : InboxProtocol 
     {
         public new PostalProtocol protocol = PostalProtocol.IMAP;
-        public IMAP(bool useSsl = true, int Port = 143, int SslPort = 993)
+        public IMAP(bool useSsl = true, int Port = 143, int SslPort = 993, string email_address = null)
         {
             this.useSsl = useSsl;
             this.Port = Port;
             this.SslPort = SslPort;
+            if (email_address != null) InboxServerName = "imap." + email_address.Split('@')[1];
         }
     }
     [Serializable]
     public class SMTP : OutboxProtocol
     {
         public new PostalProtocol protocol = PostalProtocol.SMTP;
-        public SMTP(bool useSsl = true, int Port = 587, int SslPort = 465)
+        public SMTP(bool useSsl = true, int Port = 587, int SslPort = 465, string email_address = null)
         {
             this.useSsl = useSsl;
             this.Port = Port;
             this.SslPort = SslPort;
+            if (email_address != null) OutboxServerName = "smtp." + email_address.Split('@')[1];
         }
     }
 }
