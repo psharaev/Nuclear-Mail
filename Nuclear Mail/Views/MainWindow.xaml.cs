c3dd19a1284d095adcaf7dd9ec8500f89753f9a8
pechhenka <mail@pechhenka.ru>
2019-11-03 22:21:35 +0300

    UIElement иконка почты

--- Nuclear Mail/Views/MainWindow.xaml.cs
+++ Nuclear Mail/Views/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Nuclear_Mail.Views;
+﻿using Nuclear_Mail.Models;
+using Nuclear_Mail.ViewModels;
+using Nuclear_Mail.ViewModels.Signals;
+using Nuclear_Mail.Views;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,22 +19,38 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Nuclear_Mail
+namespace Nuclear_Mail.Views
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
     public partial class MainWindow : Window , IReceive<SignalAddMailBox>
     {
+        private MailBox CurrentMailBox;
+        private static readonly UIElement MainMailBoxButton = (UIElement)Application.Current.Resources["MainMailBoxButton"];
         public MainWindow()
         {
             InitializeComponent();
+            CurrentMailBox = MailManager.GetMailBoxes()[0];
+            MailManager.GetMailBoxes().ForEach(x => AddMailBoxButton(x));
+            ProcessingSignals.Instance.Add(this);
+        }
+
+        public void AddMailBoxButton(MailBox item)
+        {
+            if (item == null) return;
+
+            Button button = (Button)MainMailBoxButton.CloneViaXamlSerialization();
+            button.Content = char.ToUpper(item.email_address[0]);
+            button.ToolTip = item.email_address;
+            MainMailBoxesIcon.Children.Add(button);
+            
+            MailController.CreateClient(item);
         }
 
         private void WriteLetter(object sender,RoutedEventArgs e)
         {
-            WriteLetter win = new WriteLetter();
+            WriteLetter win = new WriteLetter(CurrentMailBox);
 
             win.Show();
         }
